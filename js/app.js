6b2e7353defea5f5d5ccd111a0a88fdc3deeb592
marellanorero <marellanonorero@gmail.com>
2024-09-13 16:54:09 -0300

    arreglo del menú

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
-function playVideo1(){document.querySelector(".video-container").style.display="none",document.getElementById("youtube-video-1").style.display="block"}
+const toggle = document.querySelector(".toggle"),
+    menuDashboard = document.querySelector(".menu-dashboard"),
+    iconoMenu = toggle.querySelector("i"),
+    enlacesMenu = document.querySelectorAll(".enlace");
+
+function playVideo1() {
+    document.querySelector(".video-container").style.display = "none";
+    document.getElementById("youtube-video-1").style.display = "block";
+}
+
+toggle.addEventListener("click", () => {
+    menuDashboard.classList.toggle("open");
+    iconoMenu.classList.contains("bx-menu") 
+        ? iconoMenu.classList.replace("bx-menu", "bx-x") 
+        : iconoMenu.classList.replace("bx-x", "bx-menu");
+});
+
+enlacesMenu.forEach((e) => {
+    e.addEventListener("click", () => {
+        menuDashboard.classList.remove("open");
+        iconoMenu.classList.replace("bx-x", "bx-menu");
+    });
+});
