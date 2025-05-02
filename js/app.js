bb8a5dd2b2cbff185d0ac5f57de247e37cd952c3
marellanorero <marellanonorero@gmail.com>
2025-05-02 15:55:10 -0400

    arreglo en links de videos de youtube

--- js/app.js
+++ js/app.js
@@ -3,15 +3,28 @@ const toggle = document.querySelector(".toggle"),
     iconoMenu = toggle.querySelector("i"),
     enlacesMenu = document.querySelectorAll(".enlace");
 
-function playVideo1() {
-    document.querySelector(".video-container").style.display = "none";
-    document.getElementById("youtube-video-1").style.display = "block";
+function playVideo(id) {
+    // Oculta todos los videos
+    let videos = document.querySelectorAll('iframe');
+    videos.forEach(video => video.style.display = 'none');
+
+    // Muestra el video correspondiente al ID
+    let video = document.getElementById(id);
+    if (video) {
+        video.style.display = 'block';
+    }
 }
+
+document.querySelectorAll('.video-container').forEach((container, index) => {
+    container.addEventListener('click', () => {
+        playVideo('youtube-video-' + (index + 1));
+    });
+});
 
 toggle.addEventListener("click", () => {
     menuDashboard.classList.toggle("open");
-    iconoMenu.classList.contains("bx-menu") 
-        ? iconoMenu.classList.replace("bx-menu", "bx-x") 
+    iconoMenu.classList.contains("bx-menu")
+        ? iconoMenu.classList.replace("bx-menu", "bx-x")
         : iconoMenu.classList.replace("bx-x", "bx-menu");
 });
 
